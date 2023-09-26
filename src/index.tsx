fa91dbc0ca0424fc2e6a8c1dc9dc25f57dc5e6de
Jonathan Gill <jonathan.gill@appland.se>
2023-09-26 15:02:31 +0200

    Update generation

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
-import type { EmojiAlias } from './emojis';
 
+// Todo support passing a string, without being too slow?
 export type EmojiProps = { icon: any } &  Omit<React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>, 'src'>
-// export type EmojiProps = {
-//   name: EmojiAlias
-// } | { icon: any }
+
 
 export default function Emoji({ icon, ...props }: EmojiProps) {
   return <img src={icon} {...props} />
